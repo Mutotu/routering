924205044e037ee599b7ff9cfe5500fff3f687e1
mutotu <muttalip.aydemir@gmail.com>
2022-12-26 08:44:09 -0800

    added the bolier place for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
