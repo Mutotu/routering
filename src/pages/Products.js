e8fe3444c03b504bd4ec957c6592954b64e19ecd
mutotu <muttalip.aydemir@gmail.com>
2022-12-26 09:23:19 -0800

    added routers

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>Products</h1>;
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>
+          <Link to='/products/p1'>Abook</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>LOLO</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
